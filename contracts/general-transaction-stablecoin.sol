d82b4850a65a9f9014ef42f9c1b2aef93c8151ff
ayush24-eth <ayushtmsl611@gmail.com>
2023-10-04 00:28:07 +0530

    optimized contract

--- contracts/general-transaction-stablecoin.sol
+++ contracts/general-transaction-stablecoin.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract StablecoinTransaction {
     IERC20 public stablecoin;
-    event CheckBalance(string text, uint256 amount);
+    event CheckBalance(uint256 amount);
 
     constructor(address _stablecoinAddress) {
         stablecoin = IERC20(_stablecoinAddress);
@@ -16,23 +16,18 @@ contract StablecoinTransaction {
         require(_recipient != address(0), "Invalid recipient address");
         require(_amount > 0, "Amount must be greater than 0");
 
-        require(
-            stablecoin.transferFrom(msg.sender, _recipient, _amount),
-            "Transfer failed"
-        );
+        require(stablecoin.transferFrom(msg.sender, _recipient, _amount), "Transfer failed");
     }
 
     // Check the stablecoin balance of an address
     function getCoinBalance(address _account) external view returns (uint256) {
+        require(_account != address(0), "Invalid address");
         return stablecoin.balanceOf(_account);
     }
 
     function getUserBalance(address user_account) external returns (uint){
-    
-       string memory data = "User Balance is : ";
        uint user_bal = user_account.balance;
-       emit CheckBalance(data, user_bal );
+       emit CheckBalance(user_bal);
        return (user_bal);
-
     }
 }
